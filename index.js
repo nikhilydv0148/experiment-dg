b719baacad9145d4b5214a2f10d28fdb999f68c2
Nikhil Yadav <nikhilydv0148@gmail.com>
2025-09-02 16:47:30 +0530

    pushing to github

---
+++ index.js
@@ -0,0 +1,68 @@
+// const express=require('express')
+
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+let stdata = [];
+
+let nextId = 1;
+
+app.post("/students", (req, res) => {
+  const { name, rollno } = req.body;
+
+  const newStudent = { id: nextId++, name, rollno };
+  stdata.push(newStudent);
+  res.status(201).send(newStudent);
+});
+
+app.get("/students", (req, res) => {
+  res.status(201).send(stdata);
+});
+// show one student
+
+app.get("/students/:id", (req, res) => {
+  const student = stdata.find((st) => st.id === parseInt(req.params.id));
+  if (!student) {
+    return res.status(400).send("Student not found");
+  }
+
+  res.status(200).send(student);
+});
+
+// Update Student data
+
+app.put('/students/:id',(req,res)=>{
+      const student = stdata.find((st) => st.id === parseInt(req.params.id));
+     if (!student) {
+    return res.status(400).send("Student not found");
+  }
+  const {name,rollno}=req.body
+  student.name=name
+  student.rollno=rollno
+
+  res.status(200).send(student)
+
+})
+
+// Delete Student Data
+
+app.delete("/students/:id",(req,res)=>{
+    const index=stdata.findIndex(st=>st.id===parseInt(req.params.id))
+    if(index==-1){
+        return res.status(404).send("student not found")
+    }
+    stdata.splice(index,1)
+   return res.status(200).send("Deleted Successfully")
+})
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello Nikhil!")
+// })
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}...`);
+});
